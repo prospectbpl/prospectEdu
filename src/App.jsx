c6d536651b51063e69afaec643af8d7d01fe5df3
Aditya Shrivastava <aditya.shrivastava@horizonind.org>
2026-06-15 22:30:42 +0530

    update the deploy file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import {Routes, Route} from "react-router-dom";
 import Courses from "./pages/Courses";
 import CategoryCourses from "./pages/Courses/CategoryCourses";
